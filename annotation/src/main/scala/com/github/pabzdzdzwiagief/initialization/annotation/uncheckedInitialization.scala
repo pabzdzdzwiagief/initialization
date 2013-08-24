00456155c33bb0fbbbe344832f80e4ed21293bcb
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2013-08-24 13:21:28 +0200

    Remove `uncheckedInitialization`, use `unchecked`

--- annotation/src/main/scala/com/github/pabzdzdzwiagief/initialization/annotation/uncheckedInitialization.scala
+++
@@ -1,6 +0,0 @@
-package com.github.pabzdzdzwiagief.initialization.annotation
-
-/** Annotation informing the compiler that annotated expression
-  * should not be checked for access to uninitialized fields.
-  */
-final class uncheckedInitialization extends annotation.Annotation
