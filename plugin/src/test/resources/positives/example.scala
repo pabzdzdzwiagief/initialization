8a2658466efcb0ce769041fccf4cdf6be7e09811
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2013-08-28 18:19:36 +0200

    Remove useless `example` test

--- plugin/src/test/resources/positives/example.scala
+++
@@ -1,9 +0,0 @@
-// Example of how a test case can be done
-
-package localhost
-
-class example {
-  implicit def f(x: Int) = "provoke feature warning"
-}
-
-// warning: there were 1 feature warning(s); re-run with -feature for details
