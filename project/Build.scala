48ebb1b5dcf9fa2109ec13ae56b1e3c37faba7ee
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2013-08-28 18:10:34 +0200

    Bump project version to 0.5.1

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import Keys._
 
 object Build extends sbt.Build {
   val common = Defaults.defaultSettings ++ Seq (
-    version       := "0.5.0",
+    version       := "0.5.1",
     scalaVersion  := "2.10.2",
     scalacOptions := Seq(
       "-deprecation",
