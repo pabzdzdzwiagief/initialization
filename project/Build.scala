8b92580036ee1cdbac710d0445ce37d757d32fed
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2014-12-21 20:32:50 +0000

    Update scalatest to 2.11-compatible version

--- project/Build.scala
+++ project/Build.scala
@@ -53,7 +53,7 @@ object Build extends sbt.Build {
       description := "scalac plugin for initialization order checking",
       organization := "com.github.pabzdzdzwiagief",
       libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value,
-      libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.3" % "test",
       libraryDependencies += "org.springframework" % "spring-core" % "4.1.3.RELEASE" % "test"
     )
   )
--- src/test/scala/com/github/pabzdzdzwiagief/initialization/InitializationCheckingSuite.scala
+++ src/test/scala/com/github/pabzdzdzwiagief/initialization/InitializationCheckingSuite.scala
@@ -24,7 +24,7 @@ class InitializationCheckingSuite extends FunSuite with BeforeAndAfter {
       test(title) _
     }
     testOrIgnore {
-      expectResult(readExpectedOutput(path)) {
+      assertResult(readExpectedOutput(path)) {
         compile(source(path))
       }
     }
