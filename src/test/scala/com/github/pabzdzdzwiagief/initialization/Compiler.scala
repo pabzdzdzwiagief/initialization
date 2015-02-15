a4c7c25bbb3f31dcbcb6c742e01824eb73d16ba1
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2015-02-15 12:22:58 +0000

    Work around different ways empty context is shown
    Scala Compiler shows empty contexts differently in 2.10 and 2.11. Test
    output is normalized to make up for that difference.

--- src/test/scala/com/github/pabzdzdzwiagief/initialization/Compiler.scala
+++ src/test/scala/com/github/pabzdzdzwiagief/initialization/Compiler.scala
@@ -20,6 +20,8 @@ class Compiler(pluginClasses: Plugin.AnyClass*) extends (SourceFile => String) {
     * @return messages issued by compiler (compilation errors, warnings etc.).
     */
   def apply(source: SourceFile): String = {
+    def normalizeEmptyContexts(string: String) =
+      string.replaceAll("\\)\n$", "\\)")
     def removeLastNewline(string: String) =
       if (string.lastOption.exists(_ == '\n')) string.init else string
     def tabsToEightSpaces(string: String) =
@@ -28,7 +30,9 @@ class Compiler(pluginClasses: Plugin.AnyClass*) extends (SourceFile => String) {
     try {
       compilation.compileSources(List(source))
       val output = writer.getBuffer.toString
-      tabsToEightSpaces(removeLastNewline(output))
+      tabsToEightSpaces(
+        normalizeEmptyContexts(
+          removeLastNewline(output)))
     } finally {
       writer.getBuffer.setLength(0)
     }
